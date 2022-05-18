10880ffec9aa952af7a7400e4fe48dd635e97072
Renard <ludofernandez@msn.com>
2022-05-19 01:58:52 +0200

    Install minimalstic `controller/auth.js

--- router/auth.js
+++ router/auth.js
@@ -9,4 +9,4 @@
 module.exports = require('express')
   .Router()
   .get('/register', (_, res) => { res.render('register'); })
-  .post('/register', require('../controller/auth').signUp);
+  .post('/register', require('../controller/auth').register);
